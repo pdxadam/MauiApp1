a4f2f5cef5e5bc35111529d753e6ab8cb9f3deb2
Adam McLain <pdxadam@gmail.com>
2024-11-22 07:44:29 -0800

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    string text;
+    public string Text
+    {
+        get => text;
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
